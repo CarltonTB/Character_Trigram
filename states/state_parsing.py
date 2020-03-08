b5a3772078d9759e2474978ed62e29abbb5563d0
Carlton Brady <ctombrady@gmail.com>
2020-03-08 13:23:14 -0400

    Added code to retrieve state nicknames.

--- states/state_parsing.py
+++ states/state_parsing.py
@@ -23,7 +23,8 @@ class StateInfoParser:
         self.gsp_line_pattern = re.compile('([gG]ross|[Tt]otal) state product.+\\$[0-9\\.,]+.*([Tt]rillion|[BbMm]illion)')
         self.gsp_dollar_amount_pattern = re.compile('\\$[0-9\\.,]+')
         self.gsp_magnitude_pattern = re.compile('[Tt]rillion|[BbMm]illion')
-        self.nickname_pattern = re.compile('')
+        self.nickname_line_pattern = re.compile('[Nn]ickname\(s\).+</tr>.+mottos')
+        self.nickname_value_pattern = re.compile('>[a-zA-z ;()]+<')
 
     def parse_state_info(self):
         for file_name in self.files:
@@ -36,6 +37,7 @@ class StateInfoParser:
         state_name = None
         gsp_amount = None
         gsp_magnitude = None
+        nicknames = None
         for line in lines:
             title_match = self.title_pattern.search(line)
             if title_match is not None:
@@ -55,6 +57,13 @@ class StateInfoParser:
                     gsp_magnitude = magnitude_match.group()
                     print(state_name + ":", gsp_amount, gsp_magnitude)
 
+            nickname_line_match = self.nickname_line_pattern.search(line)
+            if nickname_line_match is not None:
+                nicknames_line = nickname_line_match.group()
+                nicknames_match = self.nickname_value_pattern.findall(nicknames_line)
+                for name in parse_nicknames(nicknames_match):
+                    print(name)
+
         fobj.close()
 
 
@@ -65,6 +74,46 @@ def get_wikipedia_files():
             files.remove(file_name)
 
     return files
+
+
+def parse_nicknames(nicknames_list):
+    new_list = []
+    carry = None
+    for i in range(0, len(nicknames_list)):
+        name = nicknames_list[i][1:len(nicknames_list[i])-1]
+        name = name.strip()
+        name_words = name.split(" ")
+        if len(name_words) == 1:
+            if ";" in name_words[0]:
+                # this word is part of the previous nickname
+                new_list[len(new_list)-1] = (new_list[len(new_list)-1] + " " + name).replace(";", "")
+
+            elif i == len(nicknames_list)-1:
+                # This is the last single word so it must be part of the previous nickname
+                for k in range(len(new_list)-1, 0, -1):
+                    if len(new_list[k]) > 0:
+                        new_list[k] = (new_list[k] + " " + name).replace(";", "")
+            else:
+                # this word is part of the next nickname
+                carry = name
+        else:
+            if carry is not None:
+                new_list.insert(i, carry + " " + name)
+                carry = None
+            else:
+                semi_colon_split = name.split(";")
+                if len(semi_colon_split) > 1:
+                    for j in range(0, len(semi_colon_split)):
+                        new_list.insert(i + j, semi_colon_split[j].strip())
+                else:
+                    new_list.insert(i, name.replace(";", ""))
+
+    # Remove empty entries in the list
+    for name in new_list:
+        if len(name) == 0:
+            new_list.remove(name)
+
+    return new_list
 
 
 if __name__ == "__main__":
